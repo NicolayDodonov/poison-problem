cf86d61cec894649a93d313ee1bf52fcee46c387
NicolayDodonov <95965202+NicolayDodonov@users.noreply.github.com>
2025-03-07 11:38:15 +0400

    add logger features

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,9 +1,51 @@
 package logger
 
-//todo: logger struct
+import (
+	"os"
+	"time"
+)
 
-//todo: Error()
+type Level uint8
 
-//todo: Debug()
+type Logger struct {
+	Path  string
+	Level Level
+}
 
-//todo: Info()
+const (
+	Debug Level = iota
+	Info
+	Error
+	Off = 10
+)
+
+func New(path string, level Level) *Logger {
+	return &Logger{path, level}
+}
+
+func (l *Logger) Error(str string) {
+	if l.Level <= Error {
+		t := time.Now()
+
+		msg := "[ERR] " + str + " Time:" + t.Format("02-01-2006 15:04:05")
+		_ = os.WriteFile(l.Path, []byte(msg), 0666)
+	}
+}
+
+func (l *Logger) Debug(str string) {
+	if l.Level <= Debug {
+		t := time.Now()
+
+		msg := "[DEB] " + str + " Time:" + t.Format("02-01-2006 15:04:05")
+		_ = os.WriteFile(l.Path, []byte(msg), 0666)
+	}
+}
+
+func (l *Logger) Info(str string) {
+	if l.Level <= Info {
+		t := time.Now()
+
+		msg := "[INF] " + str + " Time:" + t.Format("02-01-2006 15:04:05")
+		_ = os.WriteFile(l.Path, []byte(msg), 0666)
+	}
+}
