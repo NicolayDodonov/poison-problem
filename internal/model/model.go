5b0f3d3757f9126bda72720113aa64f920e6a190
NicolayDodonov <95965202+NicolayDodonov@users.noreply.github.com>
2025-03-10 13:06:30 +0400

    finish base work with model

--- internal/model/model.go
+++ internal/model/model.go
@@ -2,7 +2,7 @@ package model
 
 import (
 	"context"
-	"fmt"
+	"math/rand/v2"
 	"poison-problem/internal/logger"
 )
 
@@ -10,52 +10,89 @@ type Model struct {
 	Agents []*Agent
 	*World
 	*Statistic
+	*Parameters
 }
 
-func New() *Model {
-	//todo: init []*Agents
-
-	//todo: init World
-
-	//todo: init Statistic
-
-	return &Model{}
+type Parameters struct {
+	chanceFoodAppearing int
+	levelPoisonHalfFood int
 }
 
-// Run проводит исследование отдельной группы агентов на заданные условия мира в
-// структуре модели. После завершения моделирования, выводит какие то результаты
-// во вне. //todo: определить что будем возвращать
-func (m *Model) Run(ctx context.Context, logger *logger.Logger) {
+func New(countAgent, worldX, worldY int, sings ...*Sings) *Model {
+	//init []*Agents
+	agents := make([]*Agent, countAgent*len(sings))
+	for s := 0; s < len(sings); s++ {
+		for a := 0; a < countAgent; a++ {
+			agents[s*a+a] = NewAgent(worldX, worldY, sings[s])
+		}
+	}
+
+	//init World
+	world := NewWorld(worldX, worldY)
+
+	return &Model{
+		agents,
+		world,
+		&Statistic{
+			0,
+			0,
+			0,
+			Sings{
+				0, 0, 0, [2]int{0, 0}, 0, 0, 0,
+			},
+		},
+		&Parameters{
+			25,
+			50,
+		},
+	}
+}
+
+// Run starts one epoch of agent life simulation.
+// The epoch ends when the number of live agents is <= targetCountAgent.
+func (m *Model) Run(ctx context.Context, logger *logger.Logger, targetCountAgent int) {
 	for {
 		//update world resources
 		m.resourceHandler()
 
+		liveAgent := 0
 		//run all agent
 		for _, agent := range m.Agents {
 			err := agent.Run(m.World)
 			if err != nil {
-				logger.Error(fmt.Errorf("Agent ID", err).Error())
+				logger.Error(err.Error())
+			}
+			if agent.Energy > 0 {
+				liveAgent++
 			}
 		}
 
-		//update model stat
-		m.statHandler()
+		if liveAgent <= targetCountAgent {
+			break
+		}
+	}
+	//update model stat
+	m.statisticHandler()
+}
 
-		//todo: ch <- some info
-
-		//handle ctx event
-		select {
-		case <-ctx.Done():
-		default:
+func (m *Model) resourceHandler() {
+	for _, cells := range m.Map {
+		for _, cell := range cells {
+			//if in cell much food - we half this food
+			if cell.PoisonLevel > m.levelPoisonHalfFood && cell.FoodLevel > 15 {
+				cell.FoodLevel /= 2
+				continue
+			}
+			//if cell have small food - add it with a certain chance
+			if cell.FoodLevel < 10 && rand.IntN(100) >= m.chanceFoodAppearing {
+				cell.FoodLevel += 10
+			}
 		}
 	}
 }
 
-func (m *Model) resourceHandler() {
-	//todo: make smart spawn resource system
-}
-
-func (m *Model) statHandler() {
+// statisticHandler update information about
+func (m *Model) statisticHandler() {
 	food := 0
 	poison := 0
 	for _, cells := range m.Map {
@@ -64,6 +101,36 @@ func (m *Model) statHandler() {
 			poison += cell.PoisonLevel
 		}
 	}
-	m.Food = food
-	m.Poison = poison
+	m.Statistic.Food = food
+	m.Statistic.Poison = poison
+
+	m.Statistic.Sings = Sings{
+		0,
+		0,
+		0,
+		[2]int{0, 0},
+		0,
+		0,
+		0,
+	}
+	//sum sings parameters from agents
+	for _, agent := range m.Agents {
+		m.moveOrAction += agent.moveOrAction
+		m.turnOrMove += agent.turnOrMove
+		m.leftOrRight += agent.leftOrRight
+		m.eatOrClear[0] += agent.eatOrClear[0]
+		m.eatOrClear[1] += agent.eatOrClear[1]
+		m.getFood += agent.getFood
+		m.getPoison += agent.getPoison
+		m.makePoison += agent.makePoison
+	}
+	//and make avg count of all sings parameters
+	m.moveOrAction /= len(m.Agents)
+	m.turnOrMove /= len(m.Agents)
+	m.leftOrRight /= len(m.Agents)
+	m.eatOrClear[0] /= len(m.Agents)
+	m.eatOrClear[1] /= len(m.Agents)
+	m.getFood /= len(m.Agents)
+	m.getPoison /= len(m.Agents)
+	m.makePoison /= len(m.Agents)
 }
--- internal/model/statistic.go
+++ internal/model/statistic.go
@@ -3,13 +3,22 @@ package model
 import "strconv"
 
 type Statistic struct {
-	//Count food in world now
-	Food int
-	//Count poison in world now
-	Poison int
+	Food      int
+	Poison    int
+	AvgEnergy int
+	Sings
 }
 
 func (s *Statistic) String() string {
-	str := "Food: " + strconv.Itoa(s.Food) + " Poison: " + strconv.Itoa(s.Poison)
+	str := strconv.Itoa(s.Food) + "; " +
+		strconv.Itoa(s.Poison) + "; " +
+		strconv.Itoa(s.moveOrAction) + "; " +
+		strconv.Itoa(s.turnOrMove) + "; " +
+		strconv.Itoa(s.leftOrRight) + "; " +
+		strconv.Itoa(s.eatOrClear[0]) + "; " +
+		strconv.Itoa(s.eatOrClear[1]) + "; " +
+		strconv.Itoa(s.getFood) + "; " +
+		strconv.Itoa(s.getPoison) + "; " +
+		strconv.Itoa(s.makePoison) + "; "
 	return str
 }
